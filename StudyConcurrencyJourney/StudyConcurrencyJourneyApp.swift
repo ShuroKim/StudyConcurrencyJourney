7432672a3fa0ccc46f7b18fe886fdab02287872d
shuro <shuro0530@gmail.com>
2024-03-26 17:36:50 +0900

    Initial Commit

---
+++ StudyConcurrencyJourney/StudyConcurrencyJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyConcurrencyJourneyApp.swift
+//  StudyConcurrencyJourney
+//
+//  Created by Shuraw on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyConcurrencyJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
